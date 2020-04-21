16099e400605b7b016547a9c0ebb2a5591665153
jkj2449 <jkj8982@gmail.com>
2020-04-21 22:25:42 +0900

    환경변수 설정

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const config = require('./config/key')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jung:rmswnd12@boilerplate-crit4.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
