c43d0b07e05d683178186997edb9dd24d8a2aece
[SYS4U]정근중 <kjjeong3@lotte.net>
2020-04-22 17:16:11 +0900

    로그인 기능 구현

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 5000
 const config = require('./config/key')
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser)
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
@@ -18,11 +21,44 @@ const { User } = require("./models/User")
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
 
 app.post('/register', (req, res) => {
+
+
     const user = new User(req.body)
     user.save((err, doc) => {
         if(err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email}, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "해당 되는 유저정보가 없습니다."
+            })
+        }
+
+        user.comparePssword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 일치하지 않습니다."
+                })
+            }
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err)
+
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true,
+                    userId: user._id
+                })
+            })
         })
     })
 })
