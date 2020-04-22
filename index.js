231fdef96964f9cce5bcbb1241fe6438f828445e
jkj2449 <jkj8982@gmail.com>
2020-04-22 23:08:24 +0900

    인증 및 로그아웃 기능 추가

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ const express = require('express')
 const app = express()
 const port = 5000
 const config = require('./config/key')
-
 const cookieParser = require('cookie-parser')
-app.use(cookieParser())
-
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { User } = require("./models/User")
+const { auth } = require('./middleware/auth')
+
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-
-const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true, 
+    useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-const { User } = require("./models/User")
 
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
 
-app.post('/register', (req, res) => {
+app.post('api/user/register', (req, res) => {
     const user = new User(req.body)
     user.save((err, doc) => {
         if(err) {
@@ -65,6 +67,32 @@ app.post('/login', (req, res) => {
             })
         })
     })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        lastname: req.user.lastname
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({
+        _id: req.user._id
+    }, {
+        token: ""
+    }, (err, user => {
+        if(err) return res.json({
+            success: false
+        })
+
+        return res.status(200).send({
+            success: true
+        })
+    }))
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/User.js
+++ models/User.js
@@ -72,6 +72,21 @@ userSchema.methods.generateToken = function (cb) {
         cb(null, user)
     })
 }
+
+userSchema.statics.findByToken = function(token, cb) {
+    let user = this
+
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        user.findOne({
+            "_id": decoded,
+            "token": token 
+        }, function(err, user) {
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
